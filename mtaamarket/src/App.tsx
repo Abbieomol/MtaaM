454bce2ebc0987eb79ceb7c4811b4a68114a1e80
Leila Abbie <l.omol@alustudent.com>
2026-02-06 10:59:57 +0300

    fixing errors

--- mtaamarket/src/App.tsx
+++ mtaamarket/src/App.tsx
@@ -1,27 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import Customer from './pages/Customer';
-import Notifications from './pages/Notifications';
-import Search from './pages/Search';
-import Signup from './pages/Signup';
-import Vendor from './pages/Vendor';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Customer from "./pages/Customer";
+import Vendor from "./pages/Vendor";
+import Search from "./pages/Search";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Notifications from "./pages/Notifications";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/customer" element={<Customer />} />
-          <Route path="/notifications" element={<Notifications />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/vendor" element={<Vendor />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/customer" element={<Customer />} />
+        <Route path="/vendor" element={<Vendor />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/notifications" element={<Notifications />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- mtaamarket/src/main.tsx
+++ mtaamarket/src/main.tsx
@@ -1,28 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import Customer from './pages/Customer';
-import Notifications from './pages/Notifications';
-import Search from './pages/Search';
-import Signup from './pages/Signup';
-import Vendor from './pages/Vendor';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";   
+import "./App.css";     
 
-function App() {
-  return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/customer" element={<Customer />} />
-          <Route path="/notifications" element={<Notifications />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/vendor" element={<Vendor />} />
-        </Routes>
-      </Layout>
-    </Router>
-  );
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
